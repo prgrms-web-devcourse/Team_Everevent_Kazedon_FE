abcda0331a1a97ff1f385ff77e2015db76af94c1
Jengyoung <young_pallete@naver.com>
2021-12-02 21:47:33 +0900

    fix: 이미지 컨테이너 `inline-CSS` 타입 오류, `sizes` prop 경고문 수정
    현재 문제
    
    - 이미지 컨테이너에 넘겨줄 inline-CSS에 대한 타입이 미지정되어 타입 오류가 발생한다.
    - 이미지 컨테이너의 sizes의 경우 layout의 특정 조건일 때( fill, responsive) 가 아니면 경고문이 발생한다.
    
    해결 방법
    
    - 인터페이스에 CSS prop에 대한 타입을 지정해주었다.
    - 경고문이 발생하지 않도록 layout 값에 따라 조건문으로 prop을 설정해주었다.

--- src/components/atoms/ImageContainer.tsx
+++ src/components/atoms/ImageContainer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import NextImage from 'next/image';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
+import { css, Interpolation, Theme } from '@emotion/react';
 
 export interface ImageContainerProps {
   loader?: string;
@@ -24,6 +24,7 @@ export interface ImageContainerProps {
   objectFit?: 'contain' | 'cover' | 'fill' | 'none' | 'scale-down';
   layout?: 'intrinsic' | 'fixed' | 'fill' | 'responsive';
   isCircle?: boolean;
+  css?: Interpolation<Theme>;
 }
 
 const StyledImageContainer = styled.div`
@@ -77,7 +78,7 @@ const ImageContainer: React.FC<ImageContainerProps> = ({
         height="100%"
         placeholder={placeholder}
         layout={layout}
-        sizes={sizes}
+        sizes={layout === 'fill' || layout === 'responsive' ? sizes : undefined}
         objectFit={objectFit}
       />
     </StyledImageContainer>
