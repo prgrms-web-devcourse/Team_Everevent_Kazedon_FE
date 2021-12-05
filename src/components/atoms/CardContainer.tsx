72019ae772a2445bad964eb45c08fa6422d5e220
Jengyoung <young_pallete@naver.com>
2021-12-05 18:53:49 +0900

    feat: CardList 구현 - 주요 구현 설명 CardContainer을 구현하기 위해 필요한 atoms 컴포넌트입니다. CardItem들을 담는 역할을 수행하며, CardItem이 article이므로 ul이 아닌 section으로 구현하였습니다.
    - 상세 기능 설명
    1. 크게 flex여부에 따라 type을 구분합니다.
       따라서,  flex가 row인 경우, default이며, 만약 none으로 한다면, block으로 설정됩니다.
       이는 Context Margin 병합이 flex인지, block인지에 따라 다르기에 다음과 같이 설정했습니다.
    2. overflow 영역 설정이 가능합니다.
       아직 margin에 따른 세부 설정은 제대로 구현하지 않았으나, 이를 통해 특정 height만큼만 보여줄 수 있도록 overflow 및 height 설정이 가능합니다.

--- src/components/atoms/CardContainer.tsx
+++ src/components/atoms/CardContainer.tsx
@@ -4,21 +4,30 @@ import styles from '@styles/index';
 import React from 'react';
 
 export interface CardStyleProps {
-  padding: number | string;
+  padding?: number | string;
   width?: number | string;
-  margin?: number | string;
+  margin?: number;
+  cardType?: 'default' | 'box';
 }
 
+export type CardBgColorTypes =
+  | 'yellow'
+  | 'orange'
+  | 'pink'
+  | 'yellowGreen'
+  | 'mint';
+
 export interface StyledDefaultCardProps extends CardStyleProps {
-  bgColorName: 'yellow' | 'orange' | 'pink' | 'yellowGreen' | 'mint';
+  bgColorName: CardBgColorTypes;
 }
 
 export interface CardContainerProps extends CardStyleProps {
   cardType: 'default' | 'box';
-  bgColorName: 'yellow' | 'orange' | 'pink' | 'yellowGreen' | 'mint';
+  bgColorName: CardBgColorTypes;
 }
 
 const StyledDefaultCardContainer = styled('article')<StyledDefaultCardProps>`
+  box-sizing: border-box;
   height: 120px;
   filter: drop-shadow(0 4px 4px rgb(0 0 0 / 25%));
   border-radius: 16px;
@@ -35,26 +44,30 @@ const StyledDefaultCardContainer = styled('article')<StyledDefaultCardProps>`
 `;
 
 const StyledBoxCardContainer = styled('article')<CardStyleProps>`
+  box-sizing: border-box;
   height: 170px;
   background: ${styles.colors.background};
   filter: drop-shadow(0 4px 4px rgb(0 0 0 / 25%));
   border-radius: 16px;
-  ${({ width, margin, padding }) => css`
-    width: ${width
-      ? typeof width === 'string'
-        ? width
-        : `${width}px`
-      : `calc(50% - ${typeof margin === 'string' ? margin : `${margin}px`}) `};
-    padding: ${typeof padding === 'string' ? padding : `${padding}px`};
-    margin: ${typeof margin === 'string' ? margin : `${margin}px`};
-  `}
+  ${({ cardType, width, margin, padding }) => {
+    const mg = `${(margin as number) * 2}px`;
+    return css`
+      width: ${cardType === 'default'
+        ? typeof width === 'string'
+          ? width
+          : `${width}px`
+        : `calc(50% - ${mg})`};
+      padding: ${typeof padding === 'string' ? padding : `${padding}px`};
+      margin: ${typeof margin === 'string' ? margin : `${margin}px`};
+    `;
+  }}
 `;
 
 const CardContainer = ({
   cardType = 'default',
-  width,
+  width = 'auto',
   padding = 0,
-  margin,
+  margin = 0,
   bgColorName = 'orange',
 }: CardContainerProps) => {
   return cardType === 'box' ? (
