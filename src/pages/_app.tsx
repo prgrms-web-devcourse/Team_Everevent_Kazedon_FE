a1b26ffe7feaffc17f789f0411ff6a3981490273
Jengyoung <young_pallete@naver.com>
2021-12-08 01:22:47 +0900

    feat: Noto Sans KR font preload setting
    1. `preload` 적용
    2. FOIT를 다소 늘리고, FOUT을 살짝 늦춤으로써 매우 짧은 차단 기간으로 인한 불편함 해소
    3. `reset.css`설정에 맞춰 `body` 태그에 `font-family`를 상속

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css';
+import '@styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" as="font" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          as="style"
+          type="text/css"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=block"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
