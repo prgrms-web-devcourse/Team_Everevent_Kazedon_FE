6e94740d9d3d5aa7add805f6d07d2ad1778e0b31
Jengyoung <young_pallete@naver.com>
2021-12-09 18:54:10 +0900

    feat: EventCard 구현, storybook 테스트 적용
    - 커밋 설명
      1. 이벤트를 조회할 수 있는 카드인 `EventCard`를 구현하였습니다.
      2. 스토리북을 통해 테스트가 가능합니다.
    
    - 미구현 사항
      1. `react-icons`를 적용하지 않았습니다.
      2. `LikeButton`, `ReviewCount` 에 대해 별도로 컴포넌트 분리가 이루어지지 않았습니다. (추후 필요 시 리팩토링 예정)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,11 @@
-import CardContainer, {
-  CardBgColorTypes,
-} from '@components/atoms/CardContainer';
 import CardList from '@components/atoms/CardList';
-import Text from '@components/atoms/Text';
+import EventCard from '@components/domains/EventCard';
 import Header from '@components/domains/Header';
 import SortButtons, { buttonArrType } from '@components/domains/SortButtons';
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
-import styles from '@styles/index';
 import eventsData from 'fixtures/events';
 import type { NextPage } from 'next';
 
-const StyledReviewCount = styled.section`
-  position: absolute;
-  right: 20px;
-  bottom: 20px;
-  display: flex;
-`;
-const StyledLikeButton = styled.section`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  text-align: center;
-`;
-const marginBottomCSS = (width = '4') => css`
-  margin-bottom: ${width}px;
-`;
-const marginLeftCSS = (width = '4') => css`
-  margin-left: ${width}px;
-`;
-
 const MainPage: NextPage = () => {
-  const cardBgColorKeys = Object.keys(styles.cardBackgroundColors);
-  const colorLength = cardBgColorKeys.length;
   /* eslint-disable no-console */
   const buttonArr = [
     ['추천순', () => console.log('추천순')],
@@ -46,48 +19,7 @@ const MainPage: NextPage = () => {
       <SortButtons width={230} buttonArr={buttonArr} buttonMargin={16} />
       <CardList flexType="column" width={375} padding={0} margin={0}>
         {eventsData.map((data, idx) => (
-          <CardContainer
-            padding={20}
-            margin="8px 0"
-            bgColorName={cardBgColorKeys[idx % colorLength] as CardBgColorTypes}
-            cardType="default"
-            key={data.expiredAt}
-          >
-            <Text
-              block
-              size="micro"
-              css={marginBottomCSS()}
-            >{`~(${data.expiredAt})`}</Text>
-            <Text block bold css={marginBottomCSS('16')}>
-              {data.name}
-            </Text>
-            <Text
-              block
-              size="micro"
-            >{`${data.remainingParticipants}명 남음`}</Text>
-            <Text block size="micro">
-              {data.marketName}
-            </Text>
-            <StyledReviewCount>
-              <div>💬</div>
-              <Text
-                block
-                size="micro"
-                color={styles.colors.background}
-                css={marginLeftCSS()}
-              >
-                {data.reviewCount}
-              </Text>
-            </StyledReviewCount>
-            <StyledLikeButton>
-              <Text block size="large" css={marginBottomCSS()}>
-                {data.isLike ? '💗' : '🖤'}
-              </Text>
-              <Text block size="micro" color={styles.colors.background}>
-                {data.likeCount}
-              </Text>
-            </StyledLikeButton>
-          </CardContainer>
+          <EventCard eventData={data} idx={idx} />
         ))}
       </CardList>
     </div>
