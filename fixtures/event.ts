5785d166c8131106c661084d7f1430ffa6d222c2
Jengyoung <young_pallete@naver.com>
2021-12-09 17:34:16 +0900

    test: UI component 동작 test dataset 추가
    - 기타 사항
    이벤트 정보 수정(API-22)는 아직 모호하여 추가하지 않았습니다.

---
+++ fixtures/event.ts
@@ -0,0 +1,19 @@
+// TODO: 이벤트 상세 정보를 조회할 수 있다.
+const event = {
+  name: '오후 6시 이전 생맥주 400ml 1잔 무료',
+  expiredAt: '20211208',
+  marketName: '오비맥주 광진점',
+  marketDescription: '단체석 50석 준비되어 있습니다.많이 찾아주세요!',
+  eventDescription:
+    '간만에 실시하는 오비맥주 광진점 이벤트~! 다들 맥주와 함께 얼어 죽자구요 +_+',
+  isLike: true,
+  isFavorite: true,
+  pictures: [
+    {
+      url: '1234.png',
+    },
+  ],
+  isParticipated: true,
+};
+
+export default event;
---
+++ fixtures/shop.ts
@@ -0,0 +1,10 @@
+// TODO: 가게의 정보를 조회할 수 있다.
+
+const shopInfo = {
+  marketId: '오비맥주 광진점',
+  eventCount: '4',
+  likeCount: '329',
+  reviewCount: '72',
+};
+
+export default shopInfo;
