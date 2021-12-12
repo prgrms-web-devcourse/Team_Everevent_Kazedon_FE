215430a8f41b585879fb7e287b194a9c1649261d
Sunjae95 <tjswo9502@gmail.com>
2021-12-13 04:04:20 +0900

    feat: handler함수에 API추가

--- src/contexts/UserContext/actions.ts
+++ src/contexts/UserContext/actions.ts
@@ -1,36 +1,49 @@
+import onLogIn from '@axios/user/login';
+import onRegister from '@axios/user/register';
+import { LOGIN, LOGOUT, REGISTER, TOKEN } from '@utils/constantUser';
+import { removeStorage, setStorage } from '@utils/storage';
 import { Dispatch, useCallback } from 'react';
-import { LoginUserInfo, RegisterUserInfo, User } from './types';
+import { LoginUserInfo, RegisterUserInfo } from './types';
 
 const useUserProvider = (dispatch: Dispatch<any>) => {
   const handleLogIn = useCallback(
-    (userInfo: LoginUserInfo) => {
-      // 로그인 API통신 body: values
-      // 통신 이후 mock 데이터
+    async (userInfo: LoginUserInfo) => {
+      const res = await onLogIn(userInfo);
 
-      const currentUser: User = { id: 'seonjae', token: 'fdsafwe123' };
+      if (res.error.code) {
+        throw new Error(`로그인 실패${res.error.code}`);
+      }
+      // header에서 토큰이 어떻게 돌아오는지 확인해야됨
+      const { accessToken } = await res.headers;
+      const { id } = await res.data;
 
       dispatch({
-        type: 'LOG_IN',
-        user: currentUser,
+        type: LOGIN,
+        user: { id, token: accessToken },
       });
+      setStorage(TOKEN, accessToken);
     },
     [dispatch]
   );
 
   const handleRegister = useCallback(
-    (registerUserInfo: RegisterUserInfo) => {
-      // 로그인 API통신 body: values
-      // 통신 이후 mock 데이터
+    async (registerUserInfo: RegisterUserInfo) => {
+      const res = await onRegister(registerUserInfo);
+
+      if (res.error.code) {
+        throw new Error(`회원가입 실패${res.error.code}`);
+      }
 
       dispatch({
-        type: 'REGISTER',
+        type: REGISTER,
       });
     },
     [dispatch]
   );
 
   const handleLogOut = useCallback(() => {
-    dispatch({ type: 'LOG_OUT' });
+    dispatch({ type: LOGOUT });
+    removeStorage(TOKEN);
   }, [dispatch]);
 
   return {
