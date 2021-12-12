04ee01ea4ea495a2d69e61a43ba657b0d7bceaea
Sunjae95 <tjswo9502@gmail.com>
2021-12-13 04:06:22 +0900

    refactor: axios 응답 data 타입선언 변경

--- src/axios/types.ts
+++ src/axios/types.ts
@@ -1,12 +1,12 @@
-import { AxiosResponse, AxiosResponseHeaders } from 'axios';
+import { AxiosResponseHeaders } from 'axios';
 
 export interface ErrorType {
   code: number | null;
   message: string | null;
 }
 
-export interface ResType {
-  data: keyof AxiosResponse;
+export interface ResType<T = any> {
+  data: T;
   headers: AxiosResponseHeaders;
   error: ErrorType;
 }
