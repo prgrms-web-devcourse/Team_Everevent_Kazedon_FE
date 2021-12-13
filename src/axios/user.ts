887e99e718e2de16c817f0823a90502b6f7d552c
Sunjae95 <tjswo9502@gmail.com>
2021-12-13 19:59:32 +0900

    feat: 중복체크 API

--- src/axios/user.ts
+++ src/axios/user.ts
@@ -7,6 +7,11 @@ interface LoginUser {
   nickname: string;
 }
 
+interface CheckInfo {
+  type: string;
+  value?: string;
+}
+
 export const onLogIn = async (userInfo: LoginUserInfo) => {
   const res: ResType<LoginUser> = await request.post(
     'ffcd9492-1fe8-476b-9eff-30074dd1c121',
@@ -28,6 +33,14 @@ export const onRegister = async (registerUserInfo: RegisterUserInfo) => {
   const res: ResType<any> = await request.post(
     '1e872d4a-2267-4f77-a5cb-4e229f5eb94c',
     registerUserInfo
+  );
+
+  return res;
+};
+
+export const onRegisterCheck = async (check: CheckInfo) => {
+  const res: ResType<any> = await request.get(
+    `/signup/check?type=${check.type}&value=${check.value}`
   );
 
   return res;
