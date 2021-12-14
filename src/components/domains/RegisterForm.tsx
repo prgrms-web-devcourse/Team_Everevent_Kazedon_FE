424257df607254128ef56a2c6b711cc4a66d84a2
Sunjae95 <tjswo9502@gmail.com>
2021-12-14 17:28:13 +0900

    feat: Prev 검증

--- src/components/domains/RegisterForm.tsx
+++ src/components/domains/RegisterForm.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { Button, Input, HeaderText, Text } from '@components/atoms';
 import useForm from '@hooks/useForm';
 import Common from '@styles/index';
 import { onRegisterCheck } from '@axios/user';
+import { text } from '@utils/constantUser';
 
 const RegisterFormContainer = styled.div`
   display: flex;
@@ -23,80 +24,85 @@ const EmailWrapper = styled.div`
   margin-top: 9px;
 `;
 
-const InputWrapper = styled.div`
+const PasswordWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  height: 120px;
+  gap: 8px;
+  height: 134px;
   margin-top: 19px;
 `;
 
 const NicknameWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  height: 70px;
   margin: 32px 0;
 `;
 
-const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-type Data = {
+interface Data {
   email?: string;
   password?: string;
   passwordCheck?: string;
   nickname?: string;
-};
+}
 
 const RegisterForm = () => {
-  const { values, errors, setErrors, handleChange, handleSubmit } =
-    useForm<Data>({
-      initialValues: {
-        email: '',
-        password: '',
-        passwordCheck: '',
-        nickname: '',
-      },
-      onSubmit: (values) => {
-        // TODO: 현재 간단한 로직만 구현하였으므로 콘솔로 처리한다.
-        /* eslint-disable no-console */
-        console.log(values);
-      },
-      validate: ({ email, password, passwordCheck, nickname }) => {
-        const newErrors: Data = {};
-        if (!email) newErrors.email = '이메일을 입력해주세요.';
-        if (!password) newErrors.password = '비밀번호를 입력해주세요.';
-        if (!passwordCheck)
-          newErrors.passwordCheck = '비밀번호 확인을 입력해주세요.';
-        if (!nickname) newErrors.nickname = '닉네임을 입력해주세요.';
-        return newErrors;
-      },
-    });
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordCheckError, setPasswordCheckError] = useState(false);
+  const [nicknameError, setNicknameError] = useState(false);
+  const { values, errors, handleChange, handleSubmit } = useForm<Data>({
+    initialValues: {
+      email: '',
+      password: '',
+      passwordCheck: '',
+      nickname: '',
+    },
+    onSubmit: (values) => {
+      // TODO: 현재 간단한 로직만 구현하였으므로 콘솔로 처리한다.
+      /* eslint-disable no-console */
+      console.log(values);
+    },
+    validate: ({ email, password, passwordCheck, nickname }) => {
+      const newErrors: Data = {};
+      if (!email) newErrors.email = '이메일을 입력해주세요.';
+      if (!password) newErrors.password = '비밀번호를 입력해주세요.';
+      if (!passwordCheck)
+        newErrors.passwordCheck = '비밀번호 확인을 입력해주세요.';
+      if (!nickname) newErrors.nickname = '닉네임을 입력해주세요.';
+      return newErrors;
+    },
+  });
 
-  const handleCheck = async () => {
-    // API value.email
-    if (values.email === '') {
-      const newErrors = { email: '이메일을 입력해주세요' };
+  const onValidate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
 
-      setErrors(newErrors);
-      return;
-    }
+    handleChange(e);
 
-    const checkInfo = {
-      type: 'email',
-      value: values.email,
-    };
-
-    const res = await onRegisterCheck(checkInfo);
-    if (res.error.code) {
-      const newErrors = { email: '이메일이 중복됩니다.' };
-      setErrors(newErrors);
-    } else {
-      const newErrors = { email: '사용가능한 이메일 입니다.' };
-      setErrors(newErrors);
+    switch (name) {
+      case text.email: {
+        if (!text.emailReg.test(value || '')) setEmailError(true);
+        else setEmailError(false);
+        return;
+      }
+      case text.password: {
+        if (!text.passwordReg.test(value || '')) setPasswordError(true);
+        else setPasswordError(false);
+        return;
+      }
+      case text.passwordCheck: {
+        if (values.password === value) setPasswordCheckError(false);
+        else setPasswordCheckError(true);
+        return;
+      }
+      case text.nickname: {
+        if (!text.nicknameReg.test(value || '')) setNicknameError(true);
+        else setNicknameError(false);
+        return;
+      }
+      default: {
+        return e;
+      }
     }
   };
 
@@ -108,8 +114,8 @@ const RegisterForm = () => {
           sizeType="small"
           placeholder="이메일"
           name="email"
-          onChange={handleChange}
-          error={false}
+          onChange={onValidate}
+          error={emailError}
         />
         {errors.email && (
           <Text
@@ -127,26 +133,28 @@ const RegisterForm = () => {
           borderRadius="20px"
           reversal
           border
-          onClick={handleCheck}
+          onClick={handleSubmit}
           bold={false}
         >
           이메일 중복 체크
         </Button>
       </EmailWrapper>
-      <InputWrapper>
+      <PasswordWrapper>
         <Input
           sizeType="small"
           placeholder="비밀번호"
+          type="password"
           name="password"
-          onChange={handleChange}
-          error={false}
+          onChange={onValidate}
+          error={passwordError}
         />
         <Input
           sizeType="small"
           placeholder="비밀번호 확인"
+          type="password"
           name="passwordCheck"
-          onChange={handleChange}
-          error={false}
+          onChange={onValidate}
+          error={passwordCheckError}
         />
         {errors.password && (
           <Text
@@ -158,14 +166,14 @@ const RegisterForm = () => {
             {errors.password}
           </Text>
         )}
-      </InputWrapper>
+      </PasswordWrapper>
       <NicknameWrapper>
         <Input
           sizeType="small"
           placeholder="닉네임"
           name="nickname"
-          onChange={handleChange}
-          error={false}
+          onChange={onValidate}
+          error={nicknameError}
         />
         {errors.nickname && (
           <Text
@@ -178,18 +186,16 @@ const RegisterForm = () => {
           </Text>
         )}
       </NicknameWrapper>
-      <ButtonWrapper>
-        <Button
-          buttonType="primary"
-          width={280}
-          height={48}
-          borderRadius="15px"
-          onClick={handleSubmit}
-          bold
-        >
-          회원가입
-        </Button>
-      </ButtonWrapper>
+      <Button
+        buttonType="primary"
+        width={280}
+        height={48}
+        borderRadius="15px"
+        onClick={handleSubmit}
+        bold
+      >
+        회원가입
+      </Button>
     </RegisterFormContainer>
   );
 };
