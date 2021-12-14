f37eef1fb47c67d7846831a28229dc64ba16a3b3
oranjik <jieun.kim0413@gmail.com>
2021-12-14 23:48:25 +0900

    merge commits

--- fixtures/shopInfo.ts
+++ fixtures/shopInfo.ts
@@ -3,9 +3,9 @@
 const shopInfo = {
   marketName: '오비맥주 광진점',
   description: '매일 새롭게 따르는 생맥주를 맛보세요~!',
-  eventCount: '4',
-  favoriteCount: '329',
-  reviewCount: '72',
+  eventCount: 4,
+  favoriteCount: 329,
+  reviewCount: 72,
 };
 
 export default shopInfo;
