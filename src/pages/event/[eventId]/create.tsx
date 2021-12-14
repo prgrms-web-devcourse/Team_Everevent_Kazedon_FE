c9028ff778fb81ccf05a831cd3568564acac1933
Jengyoung <young_pallete@naver.com>
2021-12-14 17:02:14 +0900

    feat: 리뷰 작성 페이지 구현
    구현 사항
    - [x] 사용자가 리뷰 내용을 클릭하면 리뷰내용을 작성할 수 있다.
    - [x] 사용자는 사진을 업로드할 수 있다.
    - [x] 사용자는 업로드 하기 버튼을 누르면 업로드를 할 수 있다.
    
    미구현 사항
    - [ ] 사용자가 수정 버튼을 클릭하고 들어왔으면 기존 작성했던 내용이 그대로 반영되어 있다.

--- src/pages/event/[eventId]/create.tsx
+++ src/pages/event/[eventId]/create.tsx
@@ -1,21 +1,52 @@
-import { HeaderText, MainContainer, Textarea } from '@components/atoms';
-import Upload from '@components/atoms/Upload';
+import {
+  Button,
+  HeaderText,
+  MainContainer,
+  Textarea,
+  Upload,
+} from '@components/atoms';
 import { useReview } from '@contexts/review';
 import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
 import React, { useCallback } from 'react';
 
 const TextAreaCSS = css`
   margin-bottom: 96px;
   resize: none;
 `;
+
+const SubmitButtonCSS = css`
+  display: block;
+  width: 312px;
+  margin: 0 auto;
+  margin-top: 24px;
+`;
 const ReviewCreatePage = () => {
-  const { dispatchChangeReviewContent } = useReview();
+  const router = useRouter();
+  const { eventId } = router.query;
+  const {
+    review,
+    dispatchChangeReviewContent,
+    dispatchCreateReview,
+    reviewError,
+  } = useReview();
   const onChangeContent = useCallback(
     (e, value = undefined) => {
       const { name } = e.target;
       dispatchChangeReviewContent({ name, value: value ?? e.target.value });
     },
     [dispatchChangeReviewContent]
+  );
+
+  const handleSubmit = useCallback(
+    ({ description, picture }) => {
+      if (!description || !eventId) return;
+      dispatchCreateReview({ eventId, description, picture });
+      if (!reviewError.code) {
+        router.push(`/event/${eventId}`);
+      }
+    },
+    [dispatchCreateReview, router, eventId, reviewError]
   );
 
   return (
@@ -36,10 +67,18 @@ const ReviewCreatePage = () => {
       <HeaderText level={2} marginBottom={16}>
         사진
       </HeaderText>
-      <Upload
-        uploadType="multiple"
-        dispatchEvent={dispatchChangeReviewContent}
-      />
+      <Upload uploadType="single" dispatchEvent={dispatchChangeReviewContent} />
+      <Button
+        onClick={() =>
+          handleSubmit({
+            description: review.description,
+            picture: review.picture,
+          })
+        }
+        css={SubmitButtonCSS}
+      >
+        리뷰 남기기
+      </Button>
     </MainContainer>
   );
 };
