e0672b02eea9e1eca71859c2e9d0741bc6269ddf
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-12-16 20:24:47 +0900

    refactor: text 수정

--- src/pages/owner/success.tsx
+++ src/pages/owner/success.tsx
@@ -9,8 +9,8 @@ const SuccessCSS = css`
 
 const UserChangeSuccessPage = () => {
   const SuccessProps = {
-    successMessage: '전환이 완료 되었습니다!',
-    buttonText: '전환된 내 가게 보러 가기',
+    successMessage: '전환이 완료되었습니다!',
+    buttonText: '전환된 내 가게 보러가기',
   };
   // TODO: 가게 정보 API 연동 후 라우팅 처리할 예정입니다.
   const handleMove = () => {};
