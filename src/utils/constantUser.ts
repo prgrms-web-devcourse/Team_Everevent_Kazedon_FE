6031f629a1417983459ee0505963094a69769b46
Sunjae95 <tjswo9502@gmail.com>
2021-12-16 15:27:49 +0900

    style: 상수 추가

--- src/utils/constantUser.ts
+++ src/utils/constantUser.ts
@@ -3,7 +3,8 @@ export const LOGOUT = 'LOG_OUT';
 export const REGISTER = 'REGISTER';
 export const TOKEN = 'token';
 export const text = {
-  default: '',
+  default: '8자 이상 영어, 숫자, 기호를 입력해주세요.',
+  fail: '이메일과 비밀번호를 확인해주세요.',
   email: 'email',
   emailReg: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/, //eslint-disable-line
   emailInput: '이메일을 입력해주세요.',
@@ -11,7 +12,7 @@ export const text = {
   overlapEmail: '이메일이 중복됩니다.',
   password: 'password',
   passwordCheck: 'passwordCheck',
-  passwordInput: '비밀번호를 입력해주세요.',
+  passwordInput: '비밀번호 형식을 확인해주세요.',
   passwordReg: /^[A-Za-z0-9]{6,12}$/,
   nickname: 'nickname',
   nicknameReg: /^[가-힣ㄱ-ㅎa-zA-Z0-9._ -]{2,}$/,
