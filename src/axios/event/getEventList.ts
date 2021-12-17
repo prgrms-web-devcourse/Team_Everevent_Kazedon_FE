b16367513ba9233a46205b575ade790d1c333f1e
Jengyoung <young_pallete@naver.com>
2021-12-17 17:12:42 +0900

    refactor: 실제 API 연동을 위한 getEventList 수정

--- src/axios/event/getEventList.ts
+++ src/axios/event/getEventList.ts
@@ -1,11 +1,16 @@
 import request from '@axios/index';
 import { ResType } from '@axios/types';
 import { EventListType } from '@contexts/event/types';
+import { EventListParam } from './types';
 
-const getEventList = async () => {
-  const res: ResType<EventListType> = await request.get(
-    '/96effad6-e14a-4094-9ab4-d7932bf1bc33'
-  );
+const getEventList = async (queryParams: EventListParam) => {
+  let queries = '';
+  for (const key in queryParams) {
+    if (queryParams[key as keyof EventListParam]) {
+      queries += `&${key}=${queryParams[key as keyof EventListParam]}`;
+    }
+  }
+  const res: ResType<EventListType> = await request.get(`/events${queries}`);
   return res;
 };
 
---
+++ src/axios/event/types.ts
@@ -0,0 +1,6 @@
+export interface EventListParam {
+  location: string;
+  sorting: string;
+  page: number;
+  size: number;
+}
