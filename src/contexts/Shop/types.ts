bc26d3f18e3b45524e2add6b5b6e7db3fb398de1
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-12-17 18:05:14 +0900

    feat: 이벤트 생성 폼 validation 기능 추가

--- src/contexts/Shop/types.ts
+++ src/contexts/Shop/types.ts
@@ -18,11 +18,15 @@ interface PicturesType {
   url: string;
 }
 
+// TODO: 일단 marketId, expiredAt, maxParticipants은 string으로 하고 추후 수정
 export interface ShopEventInfo {
-  marketId: number | null;
   name: string | null;
+  marketId: string | null;
   description: string | null;
-  expiredAt: string | Date;
-  maxParticipants: number | null;
+  expiredAt: string | null;
+  maxParticipants: string | null;
   pictures: PicturesType[] | [];
 }
+
+// export type EventCreateFormData = Partial<Omit<ShopEventInfo, 'pictures'>>;
+export type EventCreateFormData = Partial<ShopEventInfo>;
