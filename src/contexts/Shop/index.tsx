01aa817ee748279b3738402dc1eb038fbbf1940e
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-12-21 16:20:31 +0900

    fix: index에 name추가

--- src/contexts/Shop/index.tsx
+++ src/contexts/Shop/index.tsx
@@ -1,31 +1,21 @@
 import React, { createContext, useMemo, useReducer } from 'react';
 import useShopProvider from './actions';
-import { ShopInfoData, EventCreateFormData, ShopContextType } from './types';
+import { ShopInfoData, ShopContextType } from './types';
 import { shopContextreducer } from './reducer';
 
 const initialState: ShopInfoData = {
   marketId: '',
+  name: '',
   description: '',
   eventCount: 0,
-  favoriteCount: 0,
+  likeCount: 0,
   reviewCount: 0,
 };
 
-const eventInitialState: EventCreateFormData = {
-  name: '',
-  marketId: '',
-  description: '',
-  expiredAt: '',
-  maxParticipants: '',
-  pictures: [],
-};
-
 export const ShopContext = createContext<ShopContextType>(initialState);
-export const EventContext = createContext<ShopContextType>(initialState);
 
 export const ShopProvider = ({ children }: { children: React.ReactNode }) => {
   const [state, dispatch] = useReducer(shopContextreducer, initialState);
-  const [eventState] = useReducer(shopContextreducer, eventInitialState);
 
   const { getShopInfo, createShopEvent, dispatchChangeEventContent } =
     useShopProvider(dispatch);
@@ -33,18 +23,11 @@ export const ShopProvider = ({ children }: { children: React.ReactNode }) => {
   const value = useMemo(
     () => ({
       state,
-      eventState,
       getShopInfo,
       createShopEvent,
       dispatchChangeEventContent,
     }),
-    [
-      state,
-      eventState,
-      getShopInfo,
-      createShopEvent,
-      dispatchChangeEventContent,
-    ]
+    [state, getShopInfo, createShopEvent, dispatchChangeEventContent]
   );
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
