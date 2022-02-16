4a520f67c7fa7fd8fe0ac4b250fa53686aacad90
oranjik <jieun.kim0413@gmail.com>
2022-02-16 19:40:45 +0900

    Merge 'develop'
    Co-authored-by: 이선재 <Sunjae95@users.noreply.github.com>

---
+++ src/components/domains/ShopCard.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const ShopCardContainer = styled.div`
+  border-color: black;
+  stroke: 1px;
+`;
+
+const ShopCard = () => {
+  return <ShopCardContainer />;
+};
+
+export default ShopCard;
