f8fd81a37cb1dcd4a917a5f551ed5e47354caf64
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2022-02-24 18:49:23 +0900

    refactor: 상수 enum 타입 적용

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,20 +1,16 @@
-// TODO: 2차 스프린트 후 소문자로 리팩토링한다.
-
-export default {
-  ERROR_MSG: {
-    default: '',
-    marketNameInput: '가게 이름을 입력해주세요.',
-    marketDescriptionInput: '가게 설명을 입력해주세요.',
-    marketDescriptionFormat: '200자 미만으로 작성해주세요.',
-    marketAddressInput: '가게 주소를 입력해주세요.',
-    eventNameInput: '이벤트 이름을 입력해주세요.',
-    eventNameFormat: '20자 미만으로 작성해주세요.',
-    eventDescriptionInput: '이벤트 내용을 입력해주세요.',
-    eventDescriptionFormat: '100자 미만으로 작성해주세요.',
-    eventExpiredAtInput: '이벤트 마감일자를 입력해주세요.',
-    eventExpiredAtFormat: '마감일자는 금일이후만 가능합니다.',
-    eventMaxParticipantsInput: '최대 참여 명수를 입력해주세요.',
-    eventMaxParticipantsFormat: '100명 이내로 입력해주세요.',
-    wrongRequest: '현재 잘못된 요청 형태입니다.',
-  },
-};
+// eslint-disable-next-line no-shadow
+export enum Errors {
+  marketNameInput = '가게 이름을 입력해주세요.',
+  marketDescriptionInput = '가게 설명을 입력해주세요.',
+  marketDescriptionFormat = '200자 미만으로 작성해주세요.',
+  marketAddressInput = '가게 주소를 입력해주세요.',
+  eventNameInput = '이벤트 이름을 입력해주세요.',
+  eventNameFormat = '20자 미만으로 작성해주세요.',
+  eventDescriptionInput = '이벤트 내용을 입력해주세요.',
+  eventDescriptionFormat = '100자 미만으로 작성해주세요.',
+  eventExpiredAtInput = '이벤트 마감일자를 입력해주세요.',
+  eventExpiredAtFormat = '마감일자는 금일이후만 가능합니다.',
+  eventMaxParticipantsInput = '최대 참여 명수를 입력해주세요.',
+  eventMaxParticipantsFormat = '100명 이내로 입력해주세요.',
+  wrongRequest = '현재 잘못된 요청 형태입니다.',
+}
