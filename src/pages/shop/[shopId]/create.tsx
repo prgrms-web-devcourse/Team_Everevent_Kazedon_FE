fb306491894192277cbce202b22569fa2c659b01
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2022-03-01 00:49:24 +0900

    refactor: 이벤트 생성 폼 헤더 적용

--- src/pages/shop/[shopId]/create.tsx
+++ src/pages/shop/[shopId]/create.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { MainContainer, HeaderText } from '@components/atoms';
 import EventCreateForm from '@components/domains/EventCreateForm';
+import { Header } from '@components/domains/index';
+import { marginTop } from '@utils/computed';
 
 const EventCreatePage = () => {
   return (
-    <MainContainer paddingWidth={24} paddingHeight={96}>
-      <HeaderText level={1} marginBottom={48}>
+    <MainContainer>
+      <Header />
+      <HeaderText level={1} marginBottom={48} css={marginTop(58)}>
         이벤트 만들기
       </HeaderText>
       <EventCreateForm />
--- src/pages/shop/[shopId]/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ShopDetailPage = () => {
-  return <div>가게 상세 페이지</div>;
-};
-
-export default ShopDetailPage;
